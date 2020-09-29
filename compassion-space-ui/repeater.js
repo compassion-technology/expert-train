d6efbb33cb9190371873a86856eefab4adf8c130
Priya <pchandrasekharan@us.ci.org>
2020-09-29 16:19:21 -0600

    added api calls, media and news feed

--- compassion-space-ui/repeater.js
+++ compassion-space-ui/repeater.js
@@ -4,11 +4,27 @@ $(document).ready(function(){
           var name = 0;
         $(".message-container").empty();
         name = $('.media').attr('name');
-        $.getJSON( "https://jsonplaceholder.typicode.com/comments", {postId:name},function( data ) {
+        $.getJSON( "http://ec2co-ecsel-6n3bncmeboii-471081296.us-east-2.elb.amazonaws.com/user/01234567/messages",function( data ) {
         $.each( data, function( val ) {
-            $(".message-container").append("<div class='media text-muted pt-3'><img class='mr-3 rounded shadow-sm' src='../compassion-space-ui/images/profile.jpg' width='32' height='32'><p class='media-body pb-3 mb-0 small lh-125 border-bottom border-gray'><strong class='d-block text-gray-dark'>You</strong>This is a test " +data[val].email+ "</p></div>"); 
-            //alert(data[val].email);
-            //items.push( "<div class='media text-muted pt-3'><img class='mr-3 rounded shadow-sm' src='../compassion-space-ui/images/profile.jpg' width='32' height='32'><p class='media-body pb-3 mb-0 small lh-125 border-bottom border-gray'><strong class='d-block text-gray-dark'>You</strong>This is a test" +some.email[2]+ "</p></div>");
+          var person = data[val].from;
+          var from = "";
+          var picture = "";
+          if (person === "01234567")
+          {
+            from = "You";
+            picture = "../compassion-space-ui/images/profile.jpg";
+          }
+          else {
+            from = data[val].from;
+            picture = "../compassion-space-ui/images/larpo.png";
+          }
+          if (data[val].message.content_url != null) {
+            $(".message-container").append("<div class='media text-muted pt-3'><img class='mr-3 rounded shadow-sm' src='"+picture+"' width='32' height='32'><p class='media-body pb-3 mb-0 small lh-125 border-bottom border-gray'><strong>"+from+"</strong><br>"+data[val].message.text.english+"<br><img class='mr-3 shadow-sm' src='"+data[val].message.content_url+"' width='100' height='100'></p></div>");
+            console.log(data[val].message.content_url);
+          }
+          else{
+            $(".message-container").append("<div class='media text-muted pt-3'><img class='mr-3 rounded shadow-sm' src='"+picture+"' width='32' height='32'><p class='media-body pb-3 mb-0 small lh-125 border-bottom border-gray'><strong class='d-block text-gray-dark'>"+from+"</strong>" +data[val].message.text.english+ "</p></div>");            
+          }
   });
           });
         $(".placeholder-container").remove();
@@ -17,6 +33,6 @@ $(document).ready(function(){
       $(".send").click(function(){
           var message = $.trim($(".message").val());
           $(".message").val('');
-          $(".message-container").append("<div class='media text-muted pt-3'><img class='mr-3 rounded shadow-sm' src='../compassion-space-ui/images/profile.jpg' width='32' height='32'><p class='media-body pb-3 mb-0 small lh-125 border-bottom border-gray'><strong class='d-block text-gray-dark'>You</strong>"+message+"</p></div>");
+          $(".message-container").append("<div class='media text-muted pt-3'><img class='mr-3 rounded shadow-sm' src='"+picture+"' width='32' height='32'><p class='media-body pb-3 mb-0 small lh-125 border-bottom border-gray'><strong class='d-block text-gray-dark'>"+from+"</strong>"+message+"</p></div>");
       });
     });
