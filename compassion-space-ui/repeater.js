c366db8e54d3f6d1e44551b1c53c7dbfe471e570
Priya <pchandrasekharan@us.ci.org>
2020-10-01 07:22:26 -0600

    adding recorder

--- compassion-space-ui/repeater.js
+++ compassion-space-ui/repeater.js
@@ -10,9 +10,23 @@ $(document).ready(function () {
 
   $(".send").click(function () {
     var text = document.getElementById('message-area').innerText;
-    postMessages(groupId,text);
+    var base64 = "";
+    if ($('.image-container').length){
+      imageUrl = document.querySelector('input[type=file]').files[0].name;
+      const reader = new FileReader();
+
+  reader.addEventListener("load", function () {
+    // convert image file to base64 string
+    base64 = reader.result;
+    }, false);
+    if (imageUrl) {
+      reader.readAsDataURL(imageUrl);
+    }
+    }
+    postMessages(groupId,text,base64);
     document.getElementById('message-area').innerText = "";
-    getLastMessage(groupId);
+    getLastMessage(groupId,text);
+    $(".image-container").remove();
   });
 
   $(".post").click(function () {
@@ -24,20 +38,80 @@ $(document).ready(function () {
     else
     {
       var filename = url.split("\\").pop();
-      $(".text-container").append("<div contenteditable='false' class='border border-gray image-container' style='width:100%; padding:1em'>"+filename+"</div>");
+      $(".text-container").append("<div contenteditable='false' id='image-container' class='border border-gray image-container' style='width:100%; padding:1em'>"+filename+"</div>");
     } 
   });
+
+  //here's the code for record
+let preview = document.getElementById("preview");
+let recording = document.getElementById("recording");
+let startButton = document.getElementById("startButton");
+let stopButton = document.getElementById("stopButton");
+let downloadButton = document.getElementById("downloadButton");
+let logElement = document.getElementById("log");
+
+let recordingTimeMS = 5000;
+function log(msg) {
+  logElement.innerHTML += msg + "\n";
+}
+function wait(delayInMS) {
+  return new Promise(resolve => setTimeout(resolve, delayInMS));
+}
+function startRecording(stream, lengthInMS) {
+  let recorder = new MediaRecorder(stream);
+  let data = [];
+ 
+  recorder.ondataavailable = event => data.push(event.data);
+  recorder.start();
+  log(recorder.state + " for " + (lengthInMS/1000) + " seconds...");
+ 
+  let stopped = new Promise((resolve, reject) => {
+    recorder.onstop = resolve;
+    recorder.onerror = event => reject(event.name);
+  });
+
+  let recorded = wait(lengthInMS).then(
+    () => recorder.state == "recording" && recorder.stop()
+  );
+ 
+  return Promise.all([
+    stopped,
+    recorded
+  ])
+  .then(() => data);
+}
+function stop(stream) {
+  stream.getTracks().forEach(track => track.stop());
+}
+startButton.addEventListener("click", function() {
+  navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+  }).then(stream => {
+    preview.srcObject = stream;
+    downloadButton.href = stream;
+    preview.captureStream = preview.captureStream || preview.mozCaptureStream;
+    return new Promise(resolve => preview.onplaying = resolve);
+  }).then(() => startRecording(preview.captureStream(), recordingTimeMS))
+  .then (recordedChunks => {
+    let recordedBlob = new Blob(recordedChunks, { type: "video/webm" });
+    recording.src = URL.createObjectURL(recordedBlob);
+    downloadButton.href = recording.src;
+    downloadButton.download = "RecordedVideo.webm";
+    
+    log("Successfully recorded " + recordedBlob.size + " bytes of " +
+        recordedBlob.type + " media.");
+  })
+  .catch(log);
+}, false);stopButton.addEventListener("click", function() {
+  stop(preview.srcObject);
+}, false);
 });
 
-function getLastMessage(groupId) {
-  $.getJSON("http://ec2co-ecsel-6n3bncmeboii-471081296.us-east-2.elb.amazonaws.com/messages?take=1&group="+groupId+"", function (data) {
-    $.each(data.sort, function (val) {
-      var person = data[val].from;
+function getLastMessage(groupId, text) {
       var from = "You";
       var picture = "../compassion-space-ui/images/profile.jpg";
-      $(".message-container").append("<div class='media text-muted pt-3'><img class='mr-3 rounded shadow-sm' src='" + picture + "' width='32' height='32'><p class='media-body pb-3 mb-0 small lh-125 border-bottom border-gray'><strong class='d-block text-gray-dark'>" + from + "</strong>" + data[val].message.text.english + "</p></div>");
-    });
-  });
+      $(".message-container").append("<div class='media text-muted pt-3'><img class='mr-3 rounded shadow-sm' src='" + picture + "' width='32' height='32'><p class='media-body pb-3 mb-0 small lh-125 border-bottom border-gray'><strong class='d-block text-gray-dark'>" + from + "</strong>" + text + "</p></div>");
 }
 
 function getMessages(groupId) {
@@ -54,7 +128,8 @@ function getMessages(groupId) {
         from = data[val].from;
         picture = "../compassion-space-ui/images/larpo.png";
       }
-      if (data[val].message.content_url != null) {
+      var content_url = data[val].message.content_url;
+      if (content_url) {
         $(".message-container").append("<div class='media text-muted pt-3'><img class='mr-3 rounded shadow-sm' src='" + picture + "' width='32' height='32'><p class='media-body pb-3 mb-0 small lh-125 border-bottom border-gray'><strong>" + from + "</strong><br>" + data[val].message.text.english + "<br><img class='mr-3 shadow-sm' src='" + data[val].message.content_url + "' width='100' height='100'></p></div>");
         console.log(data[val].message.content_url);
       }
@@ -65,13 +140,13 @@ function getMessages(groupId) {
   });
 }
 
-function postMessages(groupId, text) {
+function postMessages(groupId, text, imageUrl) {
     var someData = {
         "group_id":groupId,
         "text": {
-            "amharic": "ከእርስዎ ለመስማት በጣም ጥሩ ነው! ከሚስትዎ ቀዶ ጥገና ጋር ሁሉም ነገር በጥሩ ሁኔታ እየጸለይኩ ነው!",
             "english": text
-        }
+        },
+        "content_url": imageUrl
       }
     var stringyfiedData = JSON.stringify(someData)
     var saveData = $.ajax({
@@ -83,4 +158,7 @@ function postMessages(groupId, text) {
         console.log("save complete"); 
       }
     });
+}
+
+function record() {
 }
