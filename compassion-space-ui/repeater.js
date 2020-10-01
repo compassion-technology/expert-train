10438eeb56af2ef7b8d141216a582c762b2f7972
Priya <pchandrasekharan@us.ci.org>
2020-10-01 10:35:55 -0600

    test

--- compassion-space-ui/repeater.js
+++ compassion-space-ui/repeater.js
@@ -1,6 +1,15 @@
 $(document).ready(function () {
   $(".container-sub").hide();
   $(".media").click(function () {
+    var clicked = $(this).attr('id');
+    if (clicked == "123"){
+      $(".etonome").removeClass("highlight");
+      $(".larpo").addClass("highlight");
+    }
+    if (clicked == "456"){
+      $(".larpo").removeClass("highlight");
+      $(".etonome").addClass("highlight");
+    }
     $(".message-container").empty();
     groupId = parseInt($(this).attr('id'));
     getMessages(groupId);
@@ -8,21 +17,30 @@ $(document).ready(function () {
     $(".container-sub").show();
   });
 
+  $(".view").click(function () {
+    
+    var type = $(this).attr('id');
+    if (type == "sponsor") {
+        $(".sponsor").remove();
+        $(".beneficiary").remove();
+      $(".nav-underline").append("<a class='nav-link beneficiary' href='#'>Sponsored Child/Children<span class='badge badge-pill bg-light align-text-bottom'>2</span></a>");
+    }
+    else {
+        $(".beneficiary").remove();
+        $(".sponsor").remove();
+      $(".nav-underline").append("<a class='nav-link sponsor' href='letters.html'>Sponsor Profile</a>");
+
+    }
+  });
+
   $(".send").click(function () {
     var text = document.getElementById('message-area').innerText;
-    var base64 = "";
-    if ($('.image-container').length){
-      imageUrl = document.querySelector('input[type=file]').files[0].name;
-      const reader = new FileReader();
-
-  reader.addEventListener("load", function () {
-    // convert image file to base64 string
-    base64 = reader.result;
-    }, false);
-    if (imageUrl) {
-      reader.readAsDataURL(imageUrl);
+    var imagepath = "";
+    if ($("#image-container").length != 0){
+      imagepath = document.getElementById('image-container').innerText;
     }
-    }
+   var base64 = "";
+   getBase64Image(imagepath);
     postMessages(groupId,text,base64);
     document.getElementById('message-area').innerText = "";
     getLastMessage(groupId,text);
@@ -116,6 +134,9 @@ function getLastMessage(groupId, text) {
 
 function getMessages(groupId) {
   $.getJSON("http://ec2co-ecsel-6n3bncmeboii-471081296.us-east-2.elb.amazonaws.com/messages?take=5&group="+groupId+"", function (data) {
+    data.sort(function(a,b) {
+      return (a.data[val].message.created - b.data[val].message.created);
+    });
     $.each(data, function (val) {
       var person = data[val].from;
       var from = "";
@@ -130,23 +151,24 @@ function getMessages(groupId) {
       }
       var content_url = data[val].message.content_url;
       if (content_url) {
-        $(".message-container").append("<div class='media text-muted pt-3'><img class='mr-3 rounded shadow-sm' src='" + picture + "' width='32' height='32'><p class='media-body pb-3 mb-0 small lh-125 border-bottom border-gray'><strong>" + from + "</strong><br>" + data[val].message.text.english + "<br><img class='mr-3 shadow-sm' src='" + data[val].message.content_url + "' width='100' height='100'></p></div>");
+        $(".message-container").append("<div class='media text-muted pt-3'><img class='mr-3 rounded shadow-sm' src='" + picture + "' width='32' height='32'><p class='media-body pb-3 mb-0 small lh-125 border-bottom border-gray'><strong>" + from + "</strong><br>" + data[val].message.text.source + "<br><img class='mr-3 shadow-sm' src='" + data[val].message.content_url + "' width='100' height='100'></p></div>");
         console.log(data[val].message.content_url);
       }
       else {
-        $(".message-container").append("<div class='media text-muted pt-3'><img class='mr-3 rounded shadow-sm' src='" + picture + "' width='32' height='32'><p class='media-body pb-3 mb-0 small lh-125 border-bottom border-gray'><strong class='d-block text-gray-dark'>" + from + "</strong>" + data[val].message.text.english + "</p></div>");
+        $(".message-container").append("<div class='media text-muted pt-3'><img class='mr-3 rounded shadow-sm' src='" + picture + "' width='32' height='32'><p class='media-body pb-3 mb-0 small lh-125 border-bottom border-gray'><strong class='d-block text-gray-dark'>" + from + "</strong>" + data[val].message.text.source + "</p></div>");
       }
     });
   });
 }
 
-function postMessages(groupId, text, imageUrl) {
+async function postMessages(groupId, text) {
+  var encodedUrl = await getBase64Image();
     var someData = {
         "group_id":groupId,
         "text": {
-            "english": text
+            "source": text
         },
-        "content_url": imageUrl
+        "content_url": encodedUrl
       }
     var stringyfiedData = JSON.stringify(someData)
     var saveData = $.ajax({
@@ -160,5 +182,36 @@ function postMessages(groupId, text, imageUrl) {
     });
 }
 
-function record() {
-}
+// function getBase64Image() {
+//   var filesSelected = document.getElementById("input-file-now").files;
+//     if (filesSelected.length > 0) {
+//       var fileToLoad = filesSelected[0];
+
+//       var fileReader = new FileReader();
+
+//       fileReader.onload = function(fileLoadedEvent) {
+//         var srcData = fileLoadedEvent.target.result; // <--- data: base64
+
+//         var newImage = document.createElement('img');
+//         newImage.src = srcData;
+
+//         document.getElementById("imgTest").innerHTML = newImage.outerHTML;
+//       }
+//       fileReader.readAsDataURL(fileToLoad);
+//     }
+// }
+const getBase64Image=() => new Promise(resolve => {
+  var filesSelected = document.getElementById("input-file-now").files;
+  var fileToLoad = filesSelected[0];
+  const reader = new window.FileReader()
+  reader.onload = () => {
+    resolve(reader.result)
+  }
+  reader.onerror = e => {
+    console.error(e)
+    window.alert(UPLOAD_ERROR_MESSAGE)
+    metrics.track('File Upload Error', { Message: e.message, Alert: UPLOAD_ERROR_MESSAGE })
+    resolve(null)
+  }
+  reader.readAsDataURL(fileToLoad)
+})
